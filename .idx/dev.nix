f0f4b7554369b902edaa6c3339d0a5b8896cab73
jp.john159 <jp.john159@gmail.com>
2024-08-27 17:35:46 +0000

    Adicionando arquivo do IDX

---
+++ .idx/dev.nix
@@ -0,0 +1,53 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+  ];
+
+  # Sets environment variables in the workspace
+  env = { };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+      "aaron-bond.better-comments"
+      "dbaeumer.vscode-eslint"
+      "esbenp.prettier-vscode"
+      "rangav.vscode-thunder-client"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "start:dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        watch-backend = "npm run start:dev";
+      };
+    };
+  };
+}
