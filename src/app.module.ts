4cddc6ef05d3f60fa6cc5ce7287d196e488c6d99
jp.john159 <jp.john159@gmail.com>
2024-08-27 20:30:05 +0000

    apagando os modulos que eu esqueci

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
 
 @Module({
     imports: [
@@ -16,8 +14,6 @@ import { DatabaseModule } from './database/database.module';
             synchronize: true,
             migrations: [__dirname + '**/migrations/**/*{.ts,.js}'],
         }),
-        UserModule,
-        DatabaseModule,
     ],
     controllers: [AppController],
     providers: [AppService],
