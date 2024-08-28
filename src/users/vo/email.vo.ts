917ae3c4e50113f56d733fc20d9007dadae5b046
Danielbb21 <50447725+Danielbb21@users.noreply.github.com>
2024-08-28 09:41:24 -0300

    Implementado Value Object de Email
    com validação do mesmo
    
    Co-Authored-By: João Pedro de Lima <48960188+jotalima72@users.noreply.github.com>

---
+++ src/users/vo/email.vo.ts
@@ -0,0 +1,24 @@
+import { BadRequestException } from "@nestjs/common";
+import { Column } from "typeorm";
+
+
+export class Email {
+  @Column()
+  private email: string;
+  
+  constructor(email: string){
+    if(!this.validate(email)){
+      throw new BadRequestException('Email não validado');
+    }
+    this.email = email;
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
+
+  private validate(email: string) :boolean{
+    let emailRegex = new RegExp('^[a-zA-Z0-9._%±]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$')
+    return emailRegex.test(email);
+  }
+}
