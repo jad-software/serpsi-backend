02f6d567f0f33fb1bd9523378613069fcf9a696c
Danielbb21 <50447725+Danielbb21@users.noreply.github.com>
2024-08-29 20:16:27 -0300

    📚docs: Documentando o loginDto
    Adicionei a validação dos campos do mesmo

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,8 +1,22 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString, Matches } from "class-validator";
 
 export class LoginDTO {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Insira um e-mail existe',
+    example: 'psi@teste.com',
+  })
+  @Matches(RegExp('^[a-zA-Z0-9._%±]+@[a-zA-Z0-9.-]+[.]+[a-zA-Z]{2,}$'), {
+    message: 'E-mail inválido'
+  })
+  @IsNotEmpty()
   email: string;
-  @ApiProperty()
+
+  @ApiProperty({
+    description: 'Insira a senha que corresponde ao E-mail',
+    example: 'Psiteste@123',
+  })
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
