91922cbdc199f98eec49e17bb22bcb3d1427be6a
jp.john159 <jp.john159@gmail.com>
2024-09-03 04:23:48 +0000

    :recycle: refactor: refatorando para que os testes de integração só executem caso esteja com as variaveis de teste

--- src/users/users.integration.spec.ts
+++ src/users/users.integration.spec.ts
@@ -8,6 +8,7 @@ import { DatabaseModule } from '../database/database.module';
 import { userProvider } from './providers/user.providers';
 import { Email } from './vo/email.vo';
 import { User } from './entities/user.entity';
+import { TEST_INTEGRATION } from '../constants';
 
 describe('UsersController (integration)', () => {
   let controller: UsersController;
@@ -24,59 +25,70 @@ describe('UsersController (integration)', () => {
     controller = module.get<UsersController>(UsersController);
     service = module.get<UsersService>(UsersService);
   });
-
-  describe('POST /users', () => {
-    it('should create a user', async () => {
-      const dto: CreateUserDto = {
-        email: 'john.doe@example.com',
-        password: 'Senha@123',
-        role: Role.PSYCHOLOGIST,
-      };
-      globalUser = await controller.create(dto);
-      const email = new Email(dto.email as string);
-      expect(globalUser).toHaveProperty('_id');
-      expect(globalUser.email).toStrictEqual(email);
-      expect(globalUser.role).toBe(dto.role);
-    });
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
-
-  describe('GET /users', () => {
-    it('should return an array of users', async () => {
-      const users = await controller.findAll();
-      globalNumberofUsers = users.length;
-      expect(globalNumberofUsers).toBeGreaterThanOrEqual(1);
-      expect(users[0]).toHaveProperty('_id');
-      expect(users[0]).toHaveProperty('_email');
-      expect(users[0]).toHaveProperty('_role');
+  
+  if (TEST_INTEGRATION) {
+    describe('POST /users', () => {
+      it('should create a user', async () => {
+        const dto: CreateUserDto = {
+          email: 'john.doe@example.com',
+          password: 'Senha@123',
+          role: Role.PSYCHOLOGIST,
+        };
+        globalUser = await controller.create(dto);
+        const email = new Email(dto.email as string);
+        expect(globalUser).toHaveProperty('_id');
+        expect(globalUser.email).toStrictEqual(email);
+        expect(globalUser.role).toBe(dto.role);
+      });
     });
-  });
 
-  describe('GET /users/:id', () => {
-    it('should return a user by id', async () => {
-      const user = await controller.findOne(globalUser.id.id);
-      expect(user).toHaveProperty('_email', { _email: 'john.doe@example.com' });
-      expect(user).toHaveProperty('_role', Role.PSYCHOLOGIST);
+    describe('GET /users', () => {
+      it('should return an array of users', async () => {
+        const users = await controller.findAll();
+        globalNumberofUsers = users.length;
+        expect(globalNumberofUsers).toBeGreaterThanOrEqual(1);
+        expect(users[0]).toHaveProperty('_id');
+        expect(users[0]).toHaveProperty('_email');
+        expect(users[0]).toHaveProperty('_role');
+      });
     });
-  });
 
-  describe('PUT /users/:id', () => {
-    it('should update a user by id', async () => {
-      const updateDto: UpdateUserDto = {
-        email: 'john.doe.updated@example.com',
-        role: Role.ADMIN,
-      };
-      const email = new Email(updateDto.email as string);
-      const updatedUser = await controller.update(globalUser.id.id, updateDto);
-      expect(updatedUser).toHaveProperty('_email', email);
-      expect(updatedUser).toHaveProperty('_role', Role.ADMIN);
+    describe('GET /users/:id', () => {
+      it('should return a user by id', async () => {
+        const user = await controller.findOne(globalUser.id.id);
+        expect(user).toHaveProperty('_email', {
+          _email: 'john.doe@example.com',
+        });
+        expect(user).toHaveProperty('_role', Role.PSYCHOLOGIST);
+      });
     });
-  });
 
-  describe('DELETE /users/:id', () => {
-    it('should remove a user by id', async () => {
-      await controller.remove(globalUser.id.id);
-      const users = await controller.findAll();
-      expect(users.length).toBe(globalNumberofUsers - 1);
+    describe('PUT /users/:id', () => {
+      it('should update a user by id', async () => {
+        const updateDto: UpdateUserDto = {
+          email: 'john.doe.updated@example.com',
+          role: Role.ADMIN,
+        };
+        const email = new Email(updateDto.email as string);
+        const updatedUser = await controller.update(
+          globalUser.id.id,
+          updateDto
+        );
+        expect(updatedUser).toHaveProperty('_email', email);
+        expect(updatedUser).toHaveProperty('_role', Role.ADMIN);
+      });
     });
-  });
+
+    describe('DELETE /users/:id', () => {
+      it('should remove a user by id', async () => {
+        await controller.remove(globalUser.id.id);
+        const users = await controller.findAll();
+        expect(users.length).toBe(globalNumberofUsers - 1);
+      });
+    });
+  }
 });
