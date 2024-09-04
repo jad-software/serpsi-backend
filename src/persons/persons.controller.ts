e9ccc6310f9ba4f90eddbcb747f4df4dfe269dad
Danielbb21 <50447725+Danielbb21@users.noreply.github.com>
2024-09-04 12:29:05 -0300

    ✨ feat: Implementando findAll, findOne e Delete
    Falta o update e os relacionamentos

--- src/persons/persons.controller.ts
+++ src/persons/persons.controller.ts
@@ -1,7 +1,7 @@
 import { CreatePersonDto } from './dto/createPerson.dto';
 import { Person } from './entities/person.enitiy';
 import { PersonsService } from './persons.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 
 @Controller('persons')
 export class PersonsController {
@@ -11,4 +11,20 @@ export class PersonsController {
   async create(@Body() createPeronDto: CreatePersonDto): Promise<Person> {
     return this.personsService.create(createPeronDto);
   }
+
+  @Get()
+  async findAll(): Promise<Person[]> {
+    return await this.personsService.findAll();
+  }
+
+  @Get(':id')
+  async findOneById(@Param('id') id: string): Promise<Person>{
+    return await this.personsService.findOneById(id);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<any> {
+    return await this.personsService.delete(id);
+  }
+
 }
--- src/persons/persons.service.ts
+++ src/persons/persons.service.ts
@@ -1,17 +1,18 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePersonDto } from './dto/createPerson.dto';
 import { Person } from './entities/person.enitiy';
 import { data_providers } from 'src/constants';
 import { Repository } from 'typeorm';
 import { Phone } from './vo/phone.vo';
 import { Cpf } from './vo/cpf.vo';
+import { Id } from 'src/entity-base/vo/id.vo';
 
 @Injectable()
 export class PersonsService {
   constructor(
     @Inject(data_providers.PERSON_REPOSITORY)
     private personRepository: Repository<Person>
-  ) {}
+  ) { }
   async create(createPersonDto: CreatePersonDto) {
     try {
       const phone = new Phone(
@@ -35,4 +36,34 @@ export class PersonsService {
       throw new BadRequestException(err?.message);
     }
   }
+  async findAll(): Promise<Person[]> {
+    try {
+      return await this.personRepository.find()
+    }
+    catch (err) {
+      throw new BadRequestException(err?.message);
+    }
+  }
+  async findOneById(id: string): Promise<Person> {
+    try {
+      const person = await this.personRepository
+        .createQueryBuilder('person')
+        .where('person._id = :id', { id })
+        .getOneOrFail();
+      return person
+    }
+    catch (err) {
+      throw new NotFoundException(err?.message);
+    }
+  }
+
+  async delete(id: string): Promise<any> {
+    try {
+      const person = await this.findOneById(id);
+      await this.personRepository.delete(person);
+    } catch (err) {
+      throw new BadRequestException(err?.message);
+    }
+  }
+ 
 }
