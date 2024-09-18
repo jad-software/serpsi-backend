dad30c33f77f7413ba0376f95752264e690d175b
Danielbb21 <50447725+Danielbb21@users.noreply.github.com>
2024-09-18 16:47:06 -0300

    :white_check_mark: test: adicionando mais testes no upload de imagem

--- test/uploadImage.e2e-spec.ts
+++ test/uploadImage.e2e-spec.ts
@@ -27,10 +27,10 @@ describe('PersonsController (e2e)', () => {
           number: "99981798"
         },
         rg: '12.345.678-9',
-        cpf:{
-          cpf:'111.184.119-50'
+        cpf: {
+          cpf: '111.184.119-50'
         },
-        
+
         address: {
           street: 'Test Street',
           zipCode: '12345',
@@ -91,5 +91,80 @@ describe('PersonsController (e2e)', () => {
       .expect(201);
 
     expect(response.body).toEqual(expect.objectContaining(createPersonDto));
+  });
+
+
+  it('should not create a person with missing field', async () => {
+    const createPersonDto = {
+      // name: 'John Doe',
+      birthdate: '1990-01-01',
+      phone: {
+        ddi: "+55",
+        ddd: "75",
+        number: "99981798"
+      },
+      rg: '12.345.678-9',
+      cpf: {
+        cpf: '111.184.119-50'
+      },
+
+      address: {
+        street: 'Test Street',
+        zipCode: '12345',
+        state: 'Test State',
+        district: 'Test District',
+        homeNumber: 123,
+        complement: 'Test Complement'
+      },
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/persons/picture')
+      .attach('profilePicture', Buffer.from(''), 'test.png')
+      .field('personData', JSON.stringify(createPersonDto))
+      .expect(400);
+    const error = {
+      error: "Bad Request",
+      message: "Validation Error in Field: name",
+      statusCode: 400,
+    }
+    expect(response.body).toEqual(expect.objectContaining(error));
+  });
+
+  it('should not create a person with profile picture not being an image format', async () => {
+    const createPersonDto = {
+      name: 'John Doe',
+      birthdate: '1990-01-01',
+      phone: {
+        ddi: "+55",
+        ddd: "75",
+        number: "99981798"
+      },
+      rg: '12.345.678-9',
+      cpf: {
+        cpf: '111.184.119-50'
+      },
+
+      address: {
+        street: 'Test Street',
+        zipCode: '12345',
+        state: 'Test State',
+        district: 'Test District',
+        homeNumber: 123,
+        complement: 'Test Complement'
+      },
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/persons/picture')
+      .attach('profilePicture', Buffer.from(''), 'test.pdf')
+      .field('personData', JSON.stringify(createPersonDto))
+      .expect(400);
+    const error = {
+      error: "Bad Request",
+      message: "Validation failed (expected type is /(jpeg|png)$/)",
+      statusCode: 400,
+    }
+    expect(response.body).toEqual(expect.objectContaining(error));
   });
 });
