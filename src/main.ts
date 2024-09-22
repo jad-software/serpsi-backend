f52a151ce0a3fb50bb786cc868ce8b75967c5ad2
João Pedro de Lima <48960188+jotalima72@users.noreply.github.com>
2024-09-22 03:12:19 -0300

    🧱 build: ajeitando a main para fazer integrações

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('SerPSI API')
     .setDescription(
@@ -28,6 +28,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
