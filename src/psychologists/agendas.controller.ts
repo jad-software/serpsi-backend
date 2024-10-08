e2a689d1bf19a48c8a7e3421ec102ade16faadb1
Danielbb21 <50447725+Danielbb21@users.noreply.github.com>
2024-10-07 21:13:16 -0300

    :sparkles: feat: feita a validação dos campos ao criar uma agenda, para não poder ser enviada vazia e nem com horários conflitantes

--- src/psychologists/agendas.controller.ts
+++ src/psychologists/agendas.controller.ts
@@ -7,17 +7,43 @@ import {
   Param,
   Delete,
   Put,
+  BadRequestException,
 } from '@nestjs/common';
 import { AgendasService } from './agendas.service';
-import { CreateAgendaDto } from './dto/create-agenda.dto';
+import { AvailableTimeDto, CreateAgendaDto } from './dto/create-agenda.dto';
 import { UpdateAgendaDto } from './dto/update-agenda.dto';
 
 @Controller('agendas')
 export class AgendasController {
-  constructor(private readonly agendasService: AgendasService) {}
+  constructor(private readonly agendasService: AgendasService) { }
+  validateAvaliableTime(agenda: CreateAgendaDto | UpdateAgendaDto) {
 
+    agenda.agendas.forEach(a => {
+      const agendasValidadas: AvailableTimeDto[] = [];
+      a._avaliableTimes.forEach(timeOfDay => {
+        if (timeOfDay._endTime.trim() === '' || timeOfDay._startTime.trim() === '') {
+          throw new BadRequestException('startTime e endTime não podem ser vazios');
+        }
+        else if (timeOfDay._endTime <= timeOfDay._startTime) {
+          throw new BadRequestException('endTime tem que ser maior que startTime');
+        }
+        if (agendasValidadas) {
+          agendasValidadas.forEach(agendaV => {
+            if (agendaV._endTime >= timeOfDay._startTime) {
+              throw new BadRequestException(
+                'startTime de uma nova agenda  tem que ser maior que o endTime da outra'
+              );
+            }
+          })
+        }
+        agendasValidadas.push(timeOfDay);
+
+      })
+    });
+  }
   @Post()
   async create(@Body() createAgendaDto: CreateAgendaDto) {
+    this.validateAvaliableTime(createAgendaDto)
     return await this.agendasService.create(createAgendaDto);
   }
 
@@ -33,6 +59,7 @@ export class AgendasController {
 
   @Put(':id')
   update(@Param('id') id: string, @Body() updateAgendaDto: UpdateAgendaDto) {
+    this.validateAvaliableTime(updateAgendaDto)
     return this.agendasService.update(id, updateAgendaDto);
   }
 
