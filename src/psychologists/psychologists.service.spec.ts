e113fa6980c486657d26de1165904a224c74b53c
Danielbb21 <50447725+Danielbb21@users.noreply.github.com>
2024-10-11 12:37:28 -0300

    :white_check_mark: test: fazendo os testes de listagem e remove da service de Psychologists

--- src/psychologists/psychologists.service.spec.ts
+++ src/psychologists/psychologists.service.spec.ts
@@ -13,6 +13,9 @@ import { Person } from '../persons/entities/person.enitiy';
 import { Role } from '../users/vo/role.enum';
 import { data_providers } from '../constants';
 import { release } from 'os';
+import { BadRequestException } from '@nestjs/common';
+import { UpdatePsychologistDto } from './dto/update-psychologist.dto';
+import { Phone } from '../persons/vo/phone.vo';
 
 describe('PsychologistsService', () => {
   let service: PsychologistsService;
@@ -33,19 +36,21 @@ describe('PsychologistsService', () => {
     leftJoinAndSelect: jest.fn().mockReturnThis(),
   };
 
+  const mockQueryRunner = {
+    startTransaction: jest.fn(),
+    commitTransaction: jest.fn(),
+    rollbackTransaction: jest.fn(),
+    connect: jest.fn(),
+    release: jest.fn(),
+    manager: {
+      save: jest.fn(), // Mock do método save
+    },
+  };
+  
   const mockRepository = {
     manager: {
       connection: {
-        createQueryRunner: jest.fn().mockReturnValue({
-          startTransaction: jest.fn(),
-          commitTransaction: jest.fn(),
-          rollbackTransaction: jest.fn(),
-          connect: jest.fn(),
-          release: jest.fn(),
-          manager: {
-            save: jest.fn(), // Adicionando o mock de save
-          },
-        }),
+        createQueryRunner: jest.fn().mockReturnValue(mockQueryRunner), // Retorna o mock do queryRunner
       },
     },
     create: jest.fn(),
@@ -54,7 +59,7 @@ describe('PsychologistsService', () => {
     findOneOrFail: jest.fn(),
     createQueryBuilder: jest.fn().mockReturnValue(mockQueryBuilder),
     update: jest.fn(),
-    delete: jest.fn(),
+    remove: jest.fn(),
   };
 
   const mockPersonsService = {
@@ -109,69 +114,148 @@ describe('PsychologistsService', () => {
   describe('create', () => {
     it('Should create and save a new Psychologist', async () => {
       const cpf = {
-        cpf: '',
+        cpf: '123.456.789-00',
       } as Cpf;
       const crp = {
-        crp: '',
-        crpLink: ''
+        crp: '00/123456',
+        crpLink: 'crpLink.com',
       } as Crp;
-
+  
       const createPsychologistDto: CreatePsychologistDto = {
         crp,
-        meetDuration: 0,
-        meetValue: 0,
-        user: {
-          email: '',
-          password: '',
-          role: Role.PSYCHOLOGIST
-        },
+        meetDuration: 60,
+        meetValue: 100,
+        user: { email: 'john@example.com', password: 'Password@123', role: Role.PSYCHOLOGIST },
         person: {
-          rg: '',
-          birthdate: undefined,
-          name: '',
-          phone: undefined,
+          rg: '98.747.153-7',
+          birthdate: new Date('1990-01-01'),
+          name: 'John Doe',
+          phone: { ddi: '+55', ddd: '71', number: '998765432' } as Phone,
           cpf,
           address: {
-            zipCode: '',
-            street: '',
-            district: '',
-            city: '',
-            state: '',
-            homeNumber: 0,
-            complement: '',
+            zipCode: '41750-001',
+            street: 'Rua Teste',
+            district: 'Bairro Teste',
+            city: 'Salvador',
+            state: 'BA',
+            homeNumber: 123,
+            complement: 'Apt 101',
           },
         },
-      }
-
+      };
+  
       const mockPsychologist = new Psychologist({});
       mockUsersService.create.mockResolvedValue({ id: { id: 'user-id' } });
       mockPersonsService.create.mockResolvedValue({ id: { id: 'person-id' } });
       mockCloudinaryService.uploadFile.mockResolvedValue({ url: 'file-url' });
-      mockRepository.save.mockResolvedValue(mockPsychologist);
-      mockRepository.manager
-      .connection.createQueryRunner()
-      .manager.save.mockResolvedValue(mockPsychologist);
-
-
+      mockQueryRunner.manager.save.mockResolvedValue(mockPsychologist); // Mock do save
+  
       const files = [
         { fieldname: 'profilePicture', originalname: 'profile.jpg' },
         { fieldname: 'crpFile', originalname: 'crp.pdf' },
         { fieldname: 'identifyfile', originalname: 'id.pdf' },
         { fieldname: 'degreeFile', originalname: 'degree.pdf' },
       ] as Express.Multer.File[];
-
+  
       const result = await service.create(
         createPsychologistDto,
         files[0], // profilePicture
         files[1], // crpFile
         files[2], // identifyfile
         files[3], // degreeFile
-      )
+      );
+  
       expect(result).toEqual(mockPsychologist);
-      expect(mockRepository.manager
-        .connection.createQueryRunner()
-        .manager.save).toHaveBeenCalledWith(expect.any(Psychologist));
+      expect(mockQueryRunner.manager.save).toHaveBeenCalledWith(expect.any(Psychologist)); // Verifica o save
+      expect(mockQueryRunner.commitTransaction).toHaveBeenCalled(); // Verifica commit
+      expect(mockQueryRunner.release).toHaveBeenCalled(); // Verifica release
+    });
+  });
+  
+  describe('findAll', () => {
+    it('Should return an array of psychologists with user and person details', async () => {
+      const mockPsychologists = [
+        {
+          id: '1',
+          user: { id: { id: 'user-id' }, person: {} },
+        },
+      ];
+  
+      mockQueryBuilder.getMany.mockResolvedValue(mockPsychologists);
+      mockPersonsService.findOneByUserId.mockResolvedValue({ id: 'person-id' });
+  
+      const result = await service.findAll();
+  
+      expect(result).toEqual(mockPsychologists);
+      expect(mockQueryBuilder.leftJoinAndSelect).toHaveBeenCalledWith('psychologist.user', 'user');
+      expect(mockQueryBuilder.getMany).toHaveBeenCalled();
+      expect(mockPersonsService.findOneByUserId).toHaveBeenCalledWith('user-id');
+    });
+  });
+
+  describe('findOne', () => {
+    it('Should return a psychologist by id with user and person details', async () => {
+      const mockPsychologist = {
+        id: '1',
+        user: { id: { id: 'user-id' }, person: {} },
+        meetValue: '100',
+      };
+  
+      mockQueryBuilder.getOneOrFail.mockResolvedValue(mockPsychologist);
+      mockPersonsService.findOneByUserId.mockResolvedValue({ id: 'person-id' });
+  
+      const result = await service.findOne('1');
+  
+      expect(result).toEqual({
+        ...mockPsychologist,
+        meetValue: 100,
+      });
+      expect(mockQueryBuilder.where).toHaveBeenCalledWith('psychologist.id = :id', { id: '1' });
+      expect(mockQueryBuilder.getOneOrFail).toHaveBeenCalled();
+      expect(mockPersonsService.findOneByUserId).toHaveBeenCalledWith('user-id');
     });
   
-  })
+    it('Should throw BadRequestException if psychologist not found', async () => {
+      mockQueryBuilder.getOneOrFail.mockRejectedValue(new Error('Not found'));
+  
+      await expect(service.findOne('1')).rejects.toThrow(BadRequestException);
+    });
+  });
+
+  
+
+  describe('remove', () => {
+    it('Should remove a psychologist and related files', async () => {
+      const mockPsychologist = {
+        id: '1',
+        user: {
+          id: { id: 'user-id' }, 
+          person: { id: { id: 'person-id' } }, 
+        },
+        crp: { crpLink: 'crpLink/file-id' },
+        identifyLink: 'identifyLink/file-id', 
+        degreeLink: 'degreeLink/file-id', 
+      };
+  
+      service.findOne = jest.fn().mockResolvedValue(mockPsychologist);  
+  
+
+      await service.remove('1');
+  
+
+      expect(mockRepository.remove).toHaveBeenCalledWith(mockPsychologist);
+  
+
+      expect(mockPersonsService.delete).toHaveBeenCalledWith('person-id');
+  
+
+      expect(mockUsersService.remove).toHaveBeenCalledWith('user-id');
+  
+
+      expect(mockCloudinaryService.deleteFileOtherThanImage).toHaveBeenCalledTimes(3);
+      expect(mockCloudinaryService.deleteFileOtherThanImage).toHaveBeenCalledWith('file-id');
+    });
+  });
+  
+  
 });
