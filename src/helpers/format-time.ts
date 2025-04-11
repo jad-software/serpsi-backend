cb6d4f9950efd700774b4cecc8f23781a403ee91
João Pedro de Lima <48960188+jotalima72@users.noreply.github.com>
2025-04-11 12:23:48 -0300

    :bug: fix: avaliableTimes verifica corretamente as sessões marcadas

--- src/helpers/format-time.ts
+++ src/helpers/format-time.ts
@@ -1,13 +1,13 @@
 /**
- * Formats a Date object into a string representation of time in UTC format (HH:MM:SS)
+ * Formats a Date object into a string representation of time in  format (HH:MM:SS)
  * Ensures that hours, minutes and seconds are padded with leading zeros when needed
  * @param time - The Date object to format
- * @returns A string in the format "HH:MM:SS" using UTC time
+ * @returns A string in the format "HH:MM:SS" using time
  */
 export function formatTime(time: Date) {
-  return (time.getUTCHours() < 10 ? '0' + time.getUTCHours() : time.getUTCHours())
+  return (time.getHours() < 10 ? '0' + time.getHours() : time.getHours())
     + ':'
-    + (time.getUTCMinutes() < 10 ? '0' + time.getUTCMinutes() : time.getUTCMinutes())
+    + (time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes())
     + ':'
-    + (time.getUTCSeconds() < 10 ? '0' + time.getUTCSeconds() : time.getUTCSeconds());
+    + (time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds());
 }
